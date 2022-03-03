6f2c8e77ee8d61d9be25a854df97213c588f9360
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-03-03 00:10:01 -0600

    adding feedback is functional, adding animations with framer-motion

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { v4 as uuidv4 } from 'uuid'
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackData from './data/FeedbackData'
 import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData)
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4()
+
+    // console.log(newFeedback)
+    setFeedback([newFeedback, ...feedback])
+  }
 
   const deleteFeedback = (id) => {
     if (window.confirm('Delete?')) {
@@ -17,6 +26,7 @@ function App() {
     <>
       <Header />
       <div className='container'>
+        <FeedbackForm handleAdd={addFeedback} />
         <FeedbackStats feedback={feedback} />
         <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
       </div>
