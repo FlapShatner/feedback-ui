d47297402f182bb3f48ee62e8c9c5512bb74ceca
FlapShatner <94399441+FlapShatner@users.noreply.github.com>
2022-03-03 21:57:54 -0600

    deploying to netlify

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -42,6 +42,11 @@ export const FeedbackProvider = ({ children }) => {
     }
   }
 
+  //Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)))
+  }
+
   //Set item to be updated
   const editFeedback = (item) => {
     setFeedbackEdit({
@@ -54,10 +59,11 @@ export const FeedbackProvider = ({ children }) => {
     <FeedbackContext.Provider
       value={{
         feedback,
+        feedbackEdit,
         deleteFeedback,
         addFeedback,
         editFeedback,
-        feedbackEdit,
+        updateFeedback,
       }}
     >
       {children}
